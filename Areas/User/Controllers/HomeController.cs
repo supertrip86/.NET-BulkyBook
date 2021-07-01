96efa413558634ba07104b11fde265f8acfb6984
Giovanni Giunta <gio.giunta.86@gmail.com>
2021-07-01 12:08:12 +0200

    Created and configured View Starts and Imports

--- Areas/User/Controllers/HomeController.cs
+++ Areas/User/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -7,8 +8,9 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace BulkyBook.Controllers
+namespace BulkyBook.Areas.User.Controllers
 {
+    [Area("User")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
