d00f3b75334c4e40476670f0ced1cff107f4228a
Giovanni Giunta <gio.giunta.86@gmail.com>
2021-07-09 18:14:18 +0200

    Product model and CRUD implementation completed

---
+++ BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBook.DataAccess.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<CoverType> CoverTypes { get; set; }
+        public DbSet<Product> Products { get; set; }
+        
+    }
+}
---
+++ BulkyBook.Utility/SD.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BulkyBook.Utility
+{
+    public static class SD
+    {
+        public const string Proc_CoverType_Create = "usp_CreateCoverType";
+        public const string Proc_CoverType_Get = "usp_GetCoverType";
+        public const string Proc_CoverType_GetAll = "usp_GetCoverTypes";
+        public const string Proc_CoverType_Update = "usp_UpdateCoverType";
+        public const string Proc_CoverType_Delete = "usp_DeleteCoverType";
+
+
+    }
+}
