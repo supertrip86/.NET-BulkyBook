2c9682c14d983b73523b6d21fbe55eaee89c5c4a
Giovanni Giunta <gio.giunta.86@gmail.com>
2021-07-15 19:36:00 +0200

    External validation completed

--- BulkyBook/Startup.cs
+++ BulkyBook/Startup.cs
@@ -40,6 +40,22 @@ namespace BulkyBook
             services.AddScoped<IUnitOfWork, UnitOfWork>();
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
             services.AddRazorPages();
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = $"/Identity/Account/Login";
+                options.LogoutPath = $"/Identity/Account/Logout";
+                options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
+            });
+            services.AddAuthentication().AddFacebook(options =>
+            {
+                options.AppId = "1998589270288877";
+                options.AppSecret = "6b59f2c3e43b6868236420447dc96a31";
+            });
+            services.AddAuthentication().AddGoogle(options => {
+                options.ClientId = "236105518477-hlqu76bt74h1i3h3p7j3526nqtnct0tl.apps.googleusercontent.com";
+                options.ClientSecret = "dN58xXDKNu2LbWTJLN0QmvgN";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
